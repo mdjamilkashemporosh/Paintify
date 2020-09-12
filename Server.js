91f654bbabce966ce6e9a6479b37c32c8079b89e
porosh2002 <jamilkashem2002@gmail.com>
2020-09-12 21:09:38 +0600

    a

--- Server.js
+++ Server.js
@@ -40,7 +40,10 @@ app.use(bodyParser.json());
 // Route
 // Get
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('/product', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.get("/geTiMagE/:id",(req,res)=>{
